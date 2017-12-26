3c34e3e5766f57e152b29be6a12a7c00f927bf38
Divya Krishnani <div.krish02@gmail.com>
2017-12-27 02:17:10 +0530

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db= "sfs";
+$db_handle = mysql_connect($servername, $username, $password,$db);
+$db_found = mysql_select_db($db, $db_handle);
 
+
+if (!$db_found) {
+	die("DATABASE not found!");
+}
+
+?>
